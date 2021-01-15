b9071f81dabcea980757e740adbc2f420bd00853
Louis Donikian <louis.donikian@gmail.com>
2021-01-15 11:36:26 +0100

    * DancingLinkSolver.cs: Ajout fichier DancingLink * Sudoku.Benchmark.csproj: Ajout fichier DancingLink * Sudoku.DancingLink.csproj: Ajout fichier DancingLink
    * Sudoku.sln: Ajout fichier DancingLink

---
+++ Sudoku.DancingLink/DancingLinkSolver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Sudoku.Core;
+
+namespace Sudoku.DancingLink
+{
+    public class DancingLinkSolver : ISudokuSolver
+    {
+        public void Solve(GrilleSudoku s)
+        {
+            
+        }
+    }
+}
