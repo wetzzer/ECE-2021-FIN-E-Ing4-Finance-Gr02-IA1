8abd94b4509c4434c13b04486efb30ef69d8b016
Jean-Sylvain Boige <jsboige@gmail.com>
2021-02-02 23:18:10 +0100

    Ajout et correction Algo Genetique + un peu de nettoyage

---
+++ Sudoku.GeneticAlgorithm/GeneticSharp/ISudokuChromosome.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections.Generic;
+using Sudoku.Core;
+
+namespace Sudoku.GeneticAlgorithmSolver
+{
+    /// <summary>
+    /// Each type of chromosome for solving a sudoku is simply required to output a list of candidate sudokus
+    /// </summary>
+    public interface ISudokuChromosome
+    {
+        IList<GrilleSudoku> GetSudokus();
+    }
+}
